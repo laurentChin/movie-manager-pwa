14348179081c759a39fb1bf9946ffaa2aaf2a6bb
Laurent Janet <laurent.janet@gmail.com>
2017-07-02 21:12:18 +0200

    adds use of MovieItem in App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { MovieItem } from './Components/MovieItem';
+import { Movies } from './Fixtures';
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.movies = Movies;
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.movies.map((movie) => {
+          return <MovieItem key={movie.id} title={movie.title} originalTitle={movie.originalTitle} cover={movie.cover}></MovieItem>
+        })}
       </div>
     );
   }
