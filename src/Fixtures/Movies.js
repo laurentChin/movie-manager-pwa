fa71f523d8c92af36e95958b11dd6ba5147a033a
Laurent Janet <laurent.janet@gmail.com>
2017-07-02 21:09:18 +0200

    creates Movies fixtures

---
+++ src/Fixtures/Movies.js
@@ -0,0 +1,16 @@
+const Movies = [
+  {
+    'id': 1234,
+    'cover': 'https://dummyimage.com/200x150/d1cfd1/fff.jpg',
+    'title': 'titre 1',
+    'originalTitle': 'titre original 1',
+  },
+  {
+    'id': 1235,
+    'cover': 'https://dummyimage.com/200x150/d1cfd1/fff.jpg',
+    'title': 'titre 2',
+    'originalTitle': 'titre original 2',
+  }
+];
+
+export default Movies;
