56af2c0ad6208a2ccbe9623683216a4186df0acf
Laurent Janet <laurent.janet@gmail.com>
2018-05-07 23:20:17 +0200

    fixes JSX camelCase attribute notation in Image

--- src/core/Components/Image.js
+++ src/core/Components/Image.js
@@ -8,7 +8,7 @@ const Image = ({src, alt}) => {
 
   return (
     <picture>
-      <source srcset={original} media="(min-width: 980px)"/>
+      <source srcSet={original} media="(min-width: 980px)"/>
       <source srcSet={medium} media="(min-width: 400px)"/>
       <img src={small} alt={alt} />
     </picture>
