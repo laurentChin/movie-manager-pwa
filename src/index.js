415b25821150372007c883f0d75955227d9dc34b
Laurent Janet <laurent.janet@gmail.com>
2018-05-11 17:32:33 +0200

    enable react app service worker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import Root from './Root';
 import './index.css';
 
+if(process.env.NODE_ENV === 'production' && "serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register('./service-worker.js')
+    .then(() => {})
+    .catch(() => {});
+}
+
 ReactDOM.render(
   <Root />,
   document.getElementById('root')
