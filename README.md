84abee16b9b0b7c73f1b0fa2af55232abfb8094d
Laurent Janet <laurent.janet@gmail.com>
2019-01-29 14:16:36 +0100

    updates README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+# /!\ WIP /!\
+This is a side-project focused on learning new technologies and experimenting ideas.
+The main pattern used in this is "Getting the shit done!"
+
 # movie-manager-pwa
 A ReactJS based pwa to manage a movie collection
---
+++ src/Auth/components/LogInForm.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+
+import "./Login.css";
+
+const logInForm = ({ isAuthenticated, handleSubmit, submitHandler, logIn }) => (
+  <>
+    <form onSubmit={handleSubmit(submitHandler(logIn))}>
+      <div className="formField">
+        <label htmlFor="email">E-mail address</label>
+        <Field name="email" component="input" type="email" required />
+      </div>
+      <div className="formField">
+        <label htmlFor="password">Password</label>
+        <Field name="password" component="input" type="password" required />
+      </div>
+      <div className="formActions">
+        <button type="submit">Log In</button>
+      </div>
+    </form>
+  </>
+);
+
+export default reduxForm({
+  form: "logIn"
+})(logInForm);
