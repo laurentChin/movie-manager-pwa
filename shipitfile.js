20c69258395265fd1820f72c8aa070a7fa029b94
Laurent Janet <laurent.janet@gmail.com>
2019-02-03 23:42:08 +0100

    cs

--- shipitfile.js
+++ shipitfile.js
@@ -1,31 +1,29 @@
 module.exports = shipit => {
-  require('shipit-deploy')(shipit);
-  require('shipit-shared')(shipit);
+  require("shipit-deploy")(shipit);
+  require("shipit-shared")(shipit);
 
   shipit.initConfig({
     default: {
-      deployTo: '/var/www/movie-manager/pwa',
-      repositoryUrl: 'https://github.com/laurentChin/movie-manager-pwa.git',
+      deployTo: "/var/www/movie-manager/pwa",
+      repositoryUrl: "https://github.com/laurentChin/movie-manager-pwa.git",
       shared: {
         overwrite: true,
-        files: [
-          './.env'
-        ]
+        files: ["./.env"]
       }
     },
     production: {
-      servers: 'laurent@kimsufi',
-      key: '~/.ssh/id_rsa.pub'
-    },
+      servers: "laurent@kimsufi",
+      key: "~/.ssh/id_rsa.pub"
+    }
   });
 
-  shipit.on('updated', () => {
-    shipit.start('install');
+  shipit.on("updated", () => {
+    shipit.start("install");
   });
 
-  shipit.blTask('install', async () => {
+  shipit.blTask("install", async () => {
     await shipit.remote(`cd ${shipit.releasePath} && yarn install`);
     await shipit.remote(`cd ${shipit.releasePath} && yarn run build`);
     await shipit.remote(`chmod +x ${shipit.releasePath}`);
   });
-}
+};
