46a116d2a83c66307693db50576f88b9eb1f48ad
Laurent Janet <laurent.janet@gmail.com>
2019-03-01 15:21:51 +0100

    process search exclusively on client side

--- src/Search/actions.js
+++ src/Search/actions.js
@@ -1,50 +1,18 @@
-import {
-  SEARCH_PENDING,
-  SEARCH_SUCCESS,
-  SEARCH_RESET,
-  SEARCH_FAILURE
-} from "./actionTypes";
-import { GraphQLClient } from "../core";
-import { queries } from "./graphql";
+import { SEARCH_SUCCESS, SEARCH_RESET } from "./actionTypes";
 
 const search = terms => {
   return (dispatch, getStore) => {
-    dispatch({
-      type: SEARCH_PENDING
-    });
-
     const searchRegEx = new RegExp(`${terms}`, "i");
     const { movies } = getStore();
 
     const matches = movies.items.filter(movie => searchRegEx.test(movie.title));
-    const ids = matches.map(movie => movie.id);
 
-    GraphQLClient.query({
-      query: queries.SEARCH,
-      variables: {
-        terms
+    dispatch({
+      type: SEARCH_SUCCESS,
+      payload: {
+        matches
       }
-    })
-      .then(response => {
-        const {
-          data: { search: results }
-        } = response;
-
-        dispatch({
-          type: SEARCH_SUCCESS,
-          payload: {
-            matches: results.filter(result => !ids.includes(result.id))
-          }
-        });
-      })
-      .catch(error => {
-        dispatch({
-          type: SEARCH_FAILURE,
-          payload: {
-            error
-          }
-        });
-      });
+    });
   };
 };
 
