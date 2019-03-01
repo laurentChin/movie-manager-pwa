fc588596ead07c3b6e31fe09e29e42c971bb7037
Laurent Janet <laurent.janet@gmail.com>
2019-03-01 15:08:22 +0100

    removes broken import

--- src/Movie/MovieRouter.js
+++ src/Movie/MovieRouter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import { CreationPage, UpdatePage, BulkImportPage } from "./";
+import { CreationPage, UpdatePage } from "./";
 
 const MovieRouter = ({ match }) => (
   <Switch>
